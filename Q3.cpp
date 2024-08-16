bbaeb5b726a50c994b58acb52550b6aa83196fef
Muskan Asthana <m24.asthana@gmail.com>
2024-08-16 21:46:50 +0530

    Create Q3.cpp

---
+++ Q3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string reverseString(string str) {
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++) {
+        swap(str[i], str[n - i - 1]);
+    }
+    return str;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    string reversedStr = reverseString(str);
+    cout << "Reversed string: " << reversedStr << endl;
+
+    return 0;
+}
